e5b795ccb7aa5ce69da6f91dc028511802cd6fcf
camilobymiledelga <104871537+camilobymiledelga@users.noreply.github.com>
2022-05-04 18:48:39 -0500

    Update esp32_rele.ino

--- esp32_rele.ino
+++ esp32_rele.ino
@@ -1,21 +1,24 @@
-#include <WiFi.h>
-#include "FirebaseESP32.h"
+#include <WiFi.h> //primero se importa las libreria de de wifi
+#include "FirebaseESP32.h" //se importa el firebase , el cual es la plataforma que se usa para recibir los datos 
 
-const char *ssid = "RED - OCAMPO 2.4"; // nombre de la red
-const char *password = "SantajuanadearcO";
+//se reciben los datos : red de wifi y contraseña de esta red 
+const char *ssid = "RED - OCAMPO 2.4"; // nombre de la red de wifi
+const char *password = "SantajuanadearcO"; //contraseña de la red de wifi
 
-const char *FIREBASE_HOST = "esp32-c6845-default-rtdb.firebaseio.com";
+// aqui se reciben los datos del firebase
+const char *FIREBASE_HOST = "esp32-c6845-default-rtdb.firebaseio.com"; //
 const char *FIREBASE_AUTH = "lsDTNsl7T1gsF7rGUMNjlr7tEDynComVQ2avyqE6";
 
-FirebaseData firebaseData;
-String nodo = "/proyecto";
-bool iterar = true;
+//se declaran las variables que se estan usando en el firebase
+FirebaseData firebaseData; 
+String nodo = "/proyecto";  
+bool iterar = true; 
 
 void setup() {
   
   Serial.begin(9600);
-  pinMode(2,OUTPUT);
-  pinMode(27,OUTPUT);
+  pinMode(2,OUTPUT); //pin 2 , este es el led que se encuentra en sp32
+  pinMode(27,OUTPUT); //pin 27, este es el que envia la informacion al rele 5v
   
   WiFi.begin(ssid, password);
   Serial.print("[Wi-Fi]...Connecting");
